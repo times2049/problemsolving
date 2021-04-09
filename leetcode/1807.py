1a924c0499c7923557d6c0b2be441fd90cee4799
times2049 <times21c@gmail.com>
2021-04-09 11:37:29 +0900

    modify 1807.py

--- leetcode/1807.py
+++ leetcode/1807.py
@@ -1,5 +1,3 @@
 class Solution:
     def evaluate(self, s: str, knowledge: List[List[str]]) -> str:
-        class Default(dict): 
-            def __missing__(self, key): return '?'
-        return s.replace('(',"{").replace(')',"}").format_map(Default(dict(knowledge)))
+        return s.replace('(',"{").replace(')',"}").format_map(defaultdict(lambda: '?', knowledge))
